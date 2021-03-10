105bae4f3444557017e19c6b504d8aad68cb16dc
alimkenz <imangazin02@mail.ru>
2021-03-10 23:49:46 +0600

    Refactoring

--- src/main/java/com/aitu/votingsystem/controller/MainPageController.java
+++ src/main/java/com/aitu/votingsystem/controller/MainPageController.java
@@ -16,7 +16,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -35,10 +38,6 @@ public class MainPageController {
     @Autowired
     private SurveyQuestionaryService surveyQuestionaryService;
 
-    @RequestMapping("mainPage")
-    public String goToMainPage(){
-        return "mainPage";
-    }
 
     @GetMapping("/")
     public String index(Model model) {
@@ -78,10 +77,5 @@ public class MainPageController {
         model.addAttribute("surveyOne", survey);
         return "survey";
     }
-
-//    @PostMapping("/submitSurvey")
-//    public String submitSurvey(){
-//
-//    }
 }
 
